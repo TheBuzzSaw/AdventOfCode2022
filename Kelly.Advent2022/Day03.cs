346d169a71ea5ae5e8a5be971215e5594086bc39
TheBuzzSaw <TheBuzzSaw@Gmail.com>
2022-12-03 12:03:06 -0700

    Day 3 part 2.

--- Kelly.Advent2022/Day03.cs
+++ Kelly.Advent2022/Day03.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace Kelly.Advent2022;
@@ -10,34 +11,52 @@ static class Day03
         // Not only does this dodge allocating an array for every single line in the file,
         // I have a nice mutable buffer to shuffle values around in.
         var bytes = File.ReadAllBytes(inputFile);
+        var triads = new List<(int s, int f)>();
 
-        int prioritySum = 0;
+        int lineNumber = 0;
+        int prioritySum1 = 0;
+        int prioritySum2 = 0;
 
         // This does mean I have to find the line boundaries myself, but oh well. Not hard.
-        int startIndex = 0;
+        int start = 0;
 
         while (true)
         {
-            while (startIndex < bytes.Length && !Tools.IsAlpha(bytes[startIndex]))
-                ++startIndex;
+            ++lineNumber;
+
+            while (start < bytes.Length && !Tools.IsAlpha(bytes[start]))
+                ++start;
             
-            if (bytes.Length <= startIndex)
+            if (bytes.Length <= start)
                 break;
 
-            int endIndex = startIndex + 1;
+            int finish = start + 1;
 
-            while (endIndex < bytes.Length && Tools.IsAlpha(bytes[endIndex]))
-                ++endIndex;
+            while (finish < bytes.Length && Tools.IsAlpha(bytes[finish]))
+                ++finish;
             
-            var line = bytes.AsSpan(startIndex, endIndex - startIndex);
-            var commonValue = GetCommonValue(line);
-            var priority = GetPriority(commonValue);
-            prioritySum += priority;
+            var line = bytes.AsSpan(start, finish - start);
+            var commonValue1 = GetCommonValue(line);
+            var priority1 = GetPriority(commonValue1);
+            prioritySum1 += priority1;
+
+            triads.Add((start, finish));
+            if (triads.Count == 3)
+            {
+                var commonValue2 = GetCommonValue(
+                    bytes.AsSpan(triads[0].s, triads[0].f - triads[0].s),
+                    bytes.AsSpan(triads[1].s, triads[1].f - triads[1].s),
+                    bytes.AsSpan(triads[2].s, triads[2].f - triads[2].s));
+                var priority2 = GetPriority(commonValue2);
+                prioritySum2 += priority2;
+                triads.Clear();
+            }
             
-            startIndex = endIndex + 1;
+            start = finish + 1;
         }
 
-        Console.WriteLine(prioritySum);
+        Console.WriteLine("Part 1: " + prioritySum1);
+        Console.WriteLine("Part 2: " + prioritySum2);
     }
 
     static int GetCommonValue(Span<byte> line)
@@ -70,6 +89,20 @@ static class Day03
                 throw new AdventException("No common value found.");
             }
         }
+    }
+
+    static int GetCommonValue(Span<byte> line1, Span<byte> line2, Span<byte> line3)
+    {
+        line2.Sort();
+        line3.Sort();
+
+        foreach (var value in line1)
+        {
+            if (0 <= line2.BinarySearch(value) && 0 <= line3.BinarySearch(value))
+                return value;
+        }
+
+        throw new AdventException("No common value found.");
     }
 
     static int GetPriority(int c)
